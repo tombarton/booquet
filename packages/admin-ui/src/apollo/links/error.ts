8cf9f20b53f48f8f5a9fd1432e2029d783fabb9a
Tom Barton <tom.r.barton@gmail.com>
2020-10-03 15:07:32 +0100

    Add comments regarding token refresh

--- packages/admin-ui/src/apollo/links/error.ts
+++ packages/admin-ui/src/apollo/links/error.ts
@@ -1,19 +1,28 @@
 import { onError } from 'apollo-link-error';
 import { browserHistory } from '../../utils/history';
 
-export const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.forEach(({ message, locations, path, extensions }) => {
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}}`
-      );
+export const errorLink = onError(
+  ({ graphQLErrors, networkError, operation }) => {
+    console.log(operation.operationName);
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({ message, locations, path, extensions }) => {
+        // TODO: This is where we'll do token refreshes. We can grab the status
+        // code via `extensions?.exception.status`. There's certain operations that
+        // we want to ignore refresh requests for (e.g. login) so we will check the
+        // operation name for that.
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}}`
+        );
 
-      // Redirect to login page if the request is unauthorized.
-      if ([401, 403].includes(extensions?.exception?.response?.statusCode)) {
-        return browserHistory.push('/login');
-      }
-    });
+        // Redirect to login page if the request is unauthorized.
+        // TODO: We should only execute this after attempting to refresh the
+        // access token
+        if ([401, 403].includes(extensions?.exception?.response?.statusCode)) {
+          return browserHistory.push('/login');
+        }
+      });
+    }
+
+    if (networkError) console.log(`[Network error]: ${networkError}`);
   }
-
-  if (networkError) console.log(`[Network error]: ${networkError}`);
-});
+);
